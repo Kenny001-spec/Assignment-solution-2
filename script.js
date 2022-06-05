520c95055fadb1d02d6341092d254d2037136439
kehinde01 <abubakarkehinde88@gmail.com>
2022-06-05 14:01:14 +0100

    initial commit

--- script.js
+++ script.js
@@ -40,13 +40,58 @@ bmwSUV.accelerate()
  *      a. Speed will reduce by 10
  *      b. Charge will increase by 1
  * 4. Chain these two methods together to see the battery charge percentages
- * 
+ *
  * HINT: Don't forget to return *this* from each method...😏
- * 
+ *
  * You are FREE to create as many additional instances to the two above as you wish.
- * 
+ *
  * Goodluck 🍀
  */
+
+
+
+// Assignment Solution
+
+
+class Suv extends Car {
+    constructor(make, speed, charge) {
+        super(make, speed)
+        this.charge = charge;
+    }
+
+    accelerate() {
+        this.speed += 20;
+        this.charge -= 2;
+
+        console.log(`${this.make} is going at ${this.speed} with the battery percentage at ${this.charge}%`)
+    }
+
+    break() {
+        this.speed -= 10;
+        this.charge += 1;
+
+        console.log(`${this.make} is going at ${this.speed} with the battery percentage at ${this.charge}%`)
+    }
+}
+
+const bmwSuv = new Suv('BMW', 140, 46);
+console.log(bmwSuv);
+bmwSuv.accelerate();
+bmwSuv.accelerate();
+bmwSuv.accelerate();
+bmwSuv.break();
+bmwSuv.break();
+bmwSuv.break();
+
+const volvoSuv = new Suv('volvo Suv', 130, 58);
+console.log(volvoSuv);
+volvoSuv.break();
+volvoSuv.break();
+volvoSuv.break();
+volvoSuv.accelerate();
+volvoSuv.accelerate();
+volvoSuv.accelerate();
+volvoSuv.accelerate();
 
 
 
@@ -57,24 +102,21 @@ class Carb extends Car {
         this.charge = charge;
     }
 
+    // methods
 
-
-    // Methods
-
-    brake() {
-        this.speed += 20;
-        this.charge -= 2
-        console.log((`${this.make} is going at ${this.speed} with the battery percentage at ${this.charge} %`));
+    break() {
+        this.speed -= 10;
+        this.charge += 1
+        console.log((`${this.make} is going at ${this.speed} with the battery percentage at ${this.charge}%`))
     }
 
 }
+
 const myCarb1 = new Carb('VOLVO SUV', 130, 58);
 console.log(myCarb1);
-myCarb1.brake();
-myCarb1.brake();
-myCarb1.brake();
-myCarb1.brake();
-myCarb1.brake();
-
-
+myCarb1.break();
+myCarb1.break();
+myCarb1.break();
+myCarb1.break();
+myCarb1.break();
 
